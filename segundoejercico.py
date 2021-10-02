e5e5c004a82c77f8218a57d8fd242dc83ffe7dec
Hectoralexa <87905201+Hectoralexan@users.noreply.github.com>
2021-10-02 10:10:03 -0600

    Update segundoejercico.py

--- segundoejercico.py
+++ segundoejercico.py
@@ -1,5 +1,3 @@
-#Para pagar impuesto un determinado impuesto se debe ser mayor de 18 años y tener unos ingresos superiores a 1000 € mensuales.
- #Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no
 
 edad = int(input("¿Cuál es tu edad? "))
 sueldo = float(input("¿Cuales son tus ingresos mensuales?"))
